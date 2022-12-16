e7e0d8b9873b4e2cea3ac817665fcf6c6edb406b
Philipp Fritsche <ph.fritsche@gmail.com>
2022-12-16 17:51:44 +0000

    test: update according to `semantic-release` types

--- test/plugin/_setup.ts
+++ test/plugin/_setup.ts
@@ -3,24 +3,39 @@ import { PluginConfig } from '../../src/semantic-release'
 
 export function setup<R>(
     callback: (config: PluginConfig, context: Context) => R,
-): {
-    exec: (config: Partial<PluginConfig>, context: Partial<Context>) => R,
-    logger: {
-        log: jest.Mock<Context['logger']['log']>,
-        error: jest.Mock<Context['logger']['error']>,
-    },
-} {
-    const logger = {
-        log: jest.fn(),
+) {
+    const logger: {
+        [k in keyof Context['logger']]: jest.Mock<Context['logger'][k]>
+    } = {
+        await: jest.fn(),
+        complete: jest.fn(),
+        debug: jest.fn(),
+        fatal: jest.fn(),
+        fav: jest.fn(),
         error: jest.fn(),
+        info: jest.fn(),
+        log: jest.fn(),
+        note: jest.fn(),
+        pause: jest.fn(),
+        pending: jest.fn(),
+        star: jest.fn(),
+        start: jest.fn(),
+        success: jest.fn(),
+        wait: jest.fn(),
+        warn: jest.fn(),
+        watch: jest.fn(),
     }
 
     return {
-        exec: (config, context) => {
-            logger.log.mockClear()
-            logger.error.mockClear()
+        exec: (config: Partial<PluginConfig>, context: Partial<Context>) => {
+            Object.values(logger).forEach(l => l.mockClear())
 
-            return callback({...config}, {logger, env: {}, ...context})
+            return callback({...config}, {
+                branch: {name: 'main'},
+                logger,
+                env: {},
+                ...context,
+            })
         },
         logger,
     }
